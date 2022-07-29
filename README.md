5a46a995f2d59a7e2665c1e1b6a8f714efcd4a64
iamtalib13 <talibsheikh16@gmail.com>
2022-07-29 21:05:27 +0530

    Added - Welcome Message

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SnakeAndLadder
+# SnakeAndLadder
--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,16 +1,7 @@
 package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadder {
-    int StartPosition=0;
-
-    public void showPosition(){
-        System.out.println("Initial position of player = "+StartPosition);
-
-    }
-
     public static void main(String[] args) {
-        SnakeAndLadder P1 = new SnakeAndLadder();
-
-        P1.showPosition();
+        System.out.println("Snake And Ladder Program");
     }
 }
