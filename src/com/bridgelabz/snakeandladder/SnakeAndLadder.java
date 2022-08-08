e7718e6ee9402faf38c601de3b36497de6876b02
iamtalib13 <talibsheikh16@gmail.com>
2022-08-08 23:43:20 +0530

    Added - play the game with 2 players

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,32 +1,49 @@
 package com.bridgelabz.snakeandladder;
 
 import java.util.Random;
+import java.util.Scanner;
+
 public class SnakeAndLadder {
     public static final int IS_NO_PLAY = 0;
     public static final int IS_LADDER = 1;
     public static final int IS_SNAKE = 2;
+
+
     int playerPosition = 0;
     int newPosition = 0;
     int diceCount = 0;
+    static int player1Play = 1;
 
     Random randomNum = new Random();
 
-    public void showPosition() {
-        if (playerPosition == 100) {
-            System.out.println("position of player = " + playerPosition);
-            System.out.println("Congratulations you are Winner !!");
-        } else {
-            System.out.println("position of player = " + playerPosition);
-        }
+    public void showPosition(int playerNo) {
 
+        System.out.println("Player " + playerNo + " Position : " + playerPosition);
+        if(playerNo==1 && playerPosition==100)
+        {
+            System.out.println("-----------------------");
+            System.out.println("Player 1 is Winner . .");
+        }
+        else if(playerNo==2 && playerPosition==100){
+            System.out.println("-----------------------");
+            System.out.println("Player 2 is Winner . .");
+        }
     }
 
     public int rollDie() {
         int dieNo = randomNum.nextInt(6) + 1;
         diceCount++;
-        System.out.println("Dice Count : "+diceCount);
+        System.out.println("Dice Count : " + diceCount);
         return dieNo;
     }
+
+    public void playGame(int playerNo) {
+        int dieNo = rollDie();
+        System.out.println("Die Number for Player " + playerNo + ": " + dieNo);
+        optionPlay(dieNo);
+        showPosition(playerNo);
+    }
+
 
     public void optionPlay(int dieNo) {
         int optionNo = randomNum.nextInt(3);
@@ -57,17 +74,38 @@ public class SnakeAndLadder {
         }
     }
 
+    public static void playerChance() {
+        if (player1Play == 1) {
+            player1Play = 0;
+        } else {
+            player1Play = 1;
+        }
+
+    }
+
+    public boolean checkWin() {
+        if (playerPosition == 100) {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
+        int checkLadder = 0;
         SnakeAndLadder P1 = new SnakeAndLadder();
-        P1.showPosition();
+        SnakeAndLadder P2 = new SnakeAndLadder();
+        P1.showPosition(1);
+        P2.showPosition(2);
 
-        while (P1.playerPosition != 100) {
-            int dieNo = P1.rollDie();
-            System.out.println("Die Number For Player : " + dieNo);
 
-            P1.optionPlay(dieNo);
-            P1.showPosition();
-            System.out.println("----------------------------------");
+        while (!P1.checkWin() && !P2.checkWin()) {
+            if (player1Play == 1) {
+                P1.playGame(1);
+            } else {
+                P2.playGame(2);
+            }
+            playerChance();
+
         }
     }
 }
